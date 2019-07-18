5eca8b58600ecf72e05db37bd052a470f4f7906b
Jorge Candeias <candeias@fastmail.fm>
2019-07-18 23:09:25 +0100

    Add project files.

---
+++ PrivateMessageReceiver/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateMessageReceiver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(configure =>
+                {
+                    configure
+                        .AddJsonFile("appsettings.json")
+                        .AddUserSecrets("09808d2f-e9a5-4846-aa48-86603ed79d88")
+                        .AddCommandLine(args);
+                })
+                .ConfigureLogging(configure => configure.AddConsole())
+                .Build();
+
+            var logger = host.Services.GetService<ILogger<Program>>();
+            var config = host.Services.GetService<IConfiguration>();
+
+            logger.LogInformation("Receiving message from the Sales Messages queue...");
+            try
+            {
+                var cs = config.GetConnectionString(config["ServiceBus:ConnectionStringName"]);
+                var name = config["ServiceBus:QueueName"];
+                var client = new QueueClient(cs, name);
+
+                var messageHandlerOptions = new MessageHandlerOptions(e => ExceptionReceivedHandler(e, logger))
+                {
+                    MaxConcurrentCalls = 1,
+                    AutoComplete = false
+                };
+
+                client.RegisterMessageHandler((m, ct) => ProcessMessagesAsync(m, client, logger), messageHandlerOptions);
+
+                Console.ReadLine();
+
+                await client.CloseAsync();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, error.Message);
+                return;
+            }
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs, ILogger<Program> logger)
+        {
+            logger.LogError(exceptionReceivedEventArgs.Exception, "Message handler encountered an exception. Context: {@Context}", exceptionReceivedEventArgs.ExceptionReceivedContext);
+            return Task.CompletedTask;
+        }
+
+        static async Task ProcessMessagesAsync(Message message, QueueClient client, ILogger<Program> logger)
+        {
+            logger.LogInformation("Received message: SequenceNumber: {@SequenceNumber} Body:{@Body}",
+                message.SystemProperties.SequenceNumber,
+                Encoding.UTF8.GetString(message.Body));
+
+            await client.CompleteAsync(message.SystemProperties.LockToken);
+        }
+    }
+}
---
+++ PrivateMessageSender/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateMessageSender
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(configure =>
+                {
+                    configure
+                        .AddJsonFile("appsettings.json")
+                        .AddUserSecrets("48079fe5-33c5-478d-9c66-fe81d5e0c398")
+                        .AddCommandLine(args);
+                })
+                .ConfigureLogging(configure => configure.AddConsole())
+                .Build();
+
+            var logger = host.Services.GetService<ILogger<Program>>();
+            var config = host.Services.GetService<IConfiguration>();
+
+            logger.LogInformation("Sending a message to the Sales Messages queue...");
+            try
+            {
+                var cs = config.GetConnectionString(config["ServiceBus:ConnectionStringName"]);
+                var name = config["ServiceBus:QueueName"];
+                var client = new QueueClient(cs, name);
+
+                var messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                logger.LogInformation("Sending message: {@Message}", messageBody);
+
+                await client.SendAsync(message);
+                await client.CloseAsync();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, error.Message);
+                return;
+            }
+            logger.LogInformation("Sent message successfully.");
+        }
+    }
+}
